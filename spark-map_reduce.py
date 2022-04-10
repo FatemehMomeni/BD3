a6cbdea2e904a307c54c3705dfc44b5ec6144f40
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2022-04-10 14:32:25 +0430

    spark mapper and reducer

---
+++ spark-map_reduce.py
@@ -0,0 +1,10 @@
+import sys
+
+from pyspark import SparkContext, SparkConf
+
+if __name__ == "__main__":
+
+    sc = SparkContext("local","Spark Word Count")
+    words = sc.textFile("/user/ebrahimi/file10G.txt").flatMap(lambda line: line.split(" "))
+    wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
+    wordCounts.saveAsTextFile("/user/fatemeh_momeni/spark_output")
