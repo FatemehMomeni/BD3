63d85db6d3e4057cb9fb8c7433dc29a7b0aa3e3f
FatemehMomeni <50900356+FatemehMomeni@users.noreply.github.com>
2022-04-10 15:53:59 +0430

    spark map_reduce

--- spark-map_reduce.py
+++ spark-map_reduce.py
@@ -5,6 +5,6 @@ from pyspark import SparkContext, SparkConf
 if __name__ == "__main__":
 
     sc = SparkContext("local","Spark Word Count")
-    words = sc.textFile("/user/ebrahimi/file10G.txt").flatMap(lambda line: line.split(" "))
+    words = sc.textFile("/user/ebrahimi/hw3-data/file1G.txt").flatMap(lambda line: line.split(" "))
     wordCounts = words.map(lambda word: (word, 1)).reduceByKey(lambda a,b:a +b)
     wordCounts.saveAsTextFile("/user/fatemeh_momeni/spark_output")
